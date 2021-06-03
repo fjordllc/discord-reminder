a943bb95533a9289ef9e641ae6e2f1873975cfe0
komagata <komagata@gmail.com>
2021-06-03 23:00:40 +0900

    rubocopの指摘点を修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,69 +1,50 @@
-class MessagesController < ApplicationController
-  before_action :set_message, only: %i[ show edit update destroy ]
+# frozen_string_literal: true
 
-  # GET /messages or /messages.json
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[show edit update destroy]
+
   def index
     @messages = Message.all
   end
 
-  # GET /messages/1 or /messages/1.json
-  def show
-  end
+  def show; end
 
-  # GET /messages/new
   def new
     @message = Message.new
   end
 
-  # GET /messages/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /messages or /messages.json
   def create
     @message = Message.new(message_params)
 
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: "Message was successfully created." }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
+    if @message.save
+      redirect_to @message, notice: 'Message was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /messages/1 or /messages/1.json
   def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: "Message was successfully updated." }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
+    if @message.update(message_params)
+      redirect_to @message, notice: 'Message was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /messages/1 or /messages/1.json
   def destroy
     @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to messages_url, notice: 'Message was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def message_params
-      params.require(:message).permit(:title, :schedule, :body, :channel_id)
-    end
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:title, :schedule, :body, :channel_id)
+  end
 end
