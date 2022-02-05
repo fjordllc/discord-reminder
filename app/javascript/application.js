efcaa71b2ad975e2ce95f8eb1c650396e068cf49
komagata <komagata@gmail.com>
2022-02-06 02:17:12 +0900

    Fix alert layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import Rails from "@rails/ujs";
 
 Rails.start();
+
+// Close alert button
+const notifications = document.querySelectorAll('.notification .delete')
+if (notifications.length > 0) {
+  notifications.forEach((notification) => {
+    notification.addEventListener('click', (n) => {
+      notification.parentNode.style.display = 'none'
+    });
+  });
+}
